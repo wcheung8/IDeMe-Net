611da944853a32c709c25ccca26a35d46c027bae
linz <linx0832@umn.edu>
2019-10-10 00:39:25 -0400

    data parallel when gpu>1

--- onlyBasetwoLoss.py
+++ onlyBasetwoLoss.py
@@ -662,7 +662,8 @@ def run():
     # if torch.cuda.device_count() > 1:
         # print("Let's use", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-    model = nn.DataParallel(model)
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
 
     model = model.cuda()
 
