adde0b951a1e6624a9a4ccf87e7e65c5f8c3c441
FuLin <linx0832@umn.edu>
2019-11-24 09:57:54 -0500

    Update oneShotBaseCls_84.py

--- datasets/oneShotBaseCls_84.py
+++ datasets/oneShotBaseCls_84.py
@@ -63,22 +63,22 @@ class miniImagenetOneshotDataset(data.Dataset):
                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                 std=[0.229, 0.224, 0.225])
                                             ])
-        # # with a inputSize of 224
+        # with a inputSize of 224
 
-        #     self.transform = transforms.Compose([filenameToPILImage,
+        # self.transform = transforms.Compose([filenameToPILImage,
+        #                                 transforms.Resize(256),
+        #                                 transforms.CenterCrop(224),
+        #                                 transforms.ToTensor(),
+        #                                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        #                                 ])
+
+        # self.galleryTransform = transforms.Compose([filenameToPILImage,
+        #                                     transforms.RandomHorizontalFlip(),
         #                                     transforms.Resize(256),
         #                                     transforms.CenterCrop(224),
         #                                     transforms.ToTensor(),
         #                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         #                                     ])
-
-        #     self.galleryTransform = transforms.Compose([filenameToPILImage,
-        #                                         transforms.RandomHorizontalFlip(),
-        #                                         transforms.Resize(256),
-        #                                         transforms.CenterCrop(224),
-        #                                         transforms.ToTensor(),
-        #                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        #                                         ])
 
 
 
